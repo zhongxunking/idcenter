7f146c6da6d76a5ca766f45cc580cb8123bf74d8
zhongxunking <zhongxunking@163.com>
2017-11-17 18:40:16 +0800

    创建项目各模块

---
+++ ids-test/src/test/java/org/antframework/ids/test/DemoTest.java
@@ -0,0 +1,24 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2017-10-05 01:14 创建
+ */
+package org.antframework.ids.test;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * 单元测试demo
+ */
+@Ignore
+public class DemoTest extends AbstractTest {
+
+    @Test
+    public void testDemo() {
+        logger.info("单元测试在此编写");
+    }
+}
