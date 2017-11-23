e99bd6f18bd137afb5f9de8c7c658083060d8011
zhongxunking <zhongxunking@163.com>
2017-11-23 23:52:39 +0800

    单元测试：修改当前周期、修改当前id

--- ids-test/src/test/java/org/antframework/ids/test/facade/api/manage/IderManageServiceTest.java
+++ ids-test/src/test/java/org/antframework/ids/test/facade/api/manage/IderManageServiceTest.java
@@ -11,11 +11,18 @@ package org.antframework.ids.test.facade.api.manage;
 import org.antframework.ids.facade.api.manage.IderManageService;
 import org.antframework.ids.facade.enums.PeriodType;
 import org.antframework.ids.facade.order.AddOrModifyIderOrder;
+import org.antframework.ids.facade.order.ModifyIderCurrentIdOrder;
+import org.antframework.ids.facade.order.ModifyIderCurrentPeriodOrder;
 import org.antframework.ids.facade.result.AddOrModifyIderResult;
+import org.antframework.ids.facade.result.ModifyIderCurrentIdResult;
+import org.antframework.ids.facade.result.ModifyIderCurrentPeriodResult;
 import org.antframework.ids.test.AbstractTest;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
 
 /**
  * id提供者管理服务单元测试
@@ -33,6 +40,26 @@ public class IderManageServiceTest extends AbstractTest {
         order.setMaxId(9000000000L);
 
         AddOrModifyIderResult result = iderManageService.addOrModifyIder(order);
+        assertSuccess(result);
+    }
+
+    @Test
+    public void testModifyIderCurrentPeriod() {
+        ModifyIderCurrentPeriodOrder order = new ModifyIderCurrentPeriodOrder();
+        order.setIdCode("oid");
+        order.setNewCurrentPeriod(DateUtils.addHours(new Date(), 2));
+
+        ModifyIderCurrentPeriodResult result = iderManageService.modifyIderCurrentPeriod(order);
+        assertSuccess(result);
+    }
+
+    @Test
+    public void testModifyIderCurrentId() {
+        ModifyIderCurrentIdOrder order = new ModifyIderCurrentIdOrder();
+        order.setIdCode("oid");
+        order.setNewCurrentId(30);
+
+        ModifyIderCurrentIdResult result = iderManageService.modifyIderCurrentId(order);
         assertSuccess(result);
     }
 }
