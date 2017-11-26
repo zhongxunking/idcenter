8b340d03e7ee7bf9dfa9a9f6d8dd8943ce87f58d
zhongxunking <zhongxunking@163.com>
2017-11-27 02:00:03 +0800

    完善修改生产者数量接口

--- ids-biz/src/main/java/org/antframework/ids/biz/service/ModifyIderProducerNumberService.java
+++ ids-biz/src/main/java/org/antframework/ids/biz/service/ModifyIderProducerNumberService.java
@@ -11,6 +11,7 @@ package org.antframework.ids.biz.service;
 import org.antframework.boot.bekit.AntBekitException;
 import org.antframework.common.util.facade.CommonResultCode;
 import org.antframework.common.util.facade.Status;
+import org.antframework.ids.biz.util.ProducerUtils;
 import org.antframework.ids.dal.dao.IderDao;
 import org.antframework.ids.dal.dao.ProducerDao;
 import org.antframework.ids.dal.entity.Ider;
@@ -42,31 +43,57 @@ public class ModifyIderProducerNumberService {
         if (ider == null) {
             throw new AntBekitException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("id提供者[%s]不存在", order.getIdCode()));
         }
-        List<Producer> producers = producerDao.findLockByIdCodeOrderByIndexAsc(ider.getIdCode());
-        for (int i = producers.size(); i < order.getNewProducerNumber(); i++) {
-            Producer sourceProducer = producers.get(i % producers.size());
-
-            Producer producer = new Producer();
-            producer.setIdCode(ider.getIdCode());
-            producer.setIndex(i);
-            producer.setCurrentPeriod(sourceProducer.getCurrentPeriod());
-            producer.setCurrentId(sourceProducer.getCurrentId() + (i - i % producers.size()));
-
-            producerDao.save(producer);
+        if (Math.max(order.getNewProducerNumber(), ider.getProducerNumber()) % Math.min(order.getNewProducerNumber(), ider.getProducerNumber()) != 0) {
+            throw new AntBekitException(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("生产者数量要么成倍增加要么成倍减少，id提供者[%s]当前生产者数量[%s]，期望数量[%s]不符合要求", order.getIdCode(), ider.getProducerNumber(), order.getNewProducerNumber()));
         }
 
-        for (int i = order.getNewProducerNumber(); i < producers.size(); i++) {
-            Producer targetProducer = producers.get(i % order.getNewProducerNumber());
-
-            Producer producer = producers.get(i);
-            if (producer.getCurrentId() > targetProducer.getCurrentId()) {
-                targetProducer.setCurrentId(producer.getCurrentId());
-                producerDao.save(producer);
-            }
-            producerDao.delete(producer);
+        if (order.getNewProducerNumber() > ider.getProducerNumber()) {
+            addProducers(ider, order.getNewProducerNumber());
+        } else if (order.getNewProducerNumber() < ider.getProducerNumber()) {
+            deleteProducers(ider, order.getNewProducerNumber());
         }
 
         ider.setProducerNumber(order.getNewProducerNumber());
         iderDao.save(ider);
+    }
+
+    // 添加id生产者
+    private void addProducers(Ider ider, int newProducerNumber) {
+        List<Producer> producers = producerDao.findLockByIdCodeOrderByIndexAsc(ider.getIdCode());
+        for (int i = ider.getProducerNumber(); i < newProducerNumber; i++) {
+            Producer producer = buildProducer(producers.get(i % ider.getProducerNumber()), i, ider);
+            producerDao.save(producer);
+        }
+    }
+
+    // 构建id生产者
+    private Producer buildProducer(Producer sourceProducer, int index, Ider ider) {
+        Producer producer = new Producer();
+        producer.setIdCode(sourceProducer.getIdCode());
+        producer.setIndex(index);
+        producer.setCurrentPeriod(sourceProducer.getCurrentPeriod());
+        producer.setCurrentId(sourceProducer.getCurrentId());
+        ProducerUtils.produce(producer, ider, index - sourceProducer.getIndex());
+
+        return producer;
+    }
+
+    // 删除id生产者
+    private void deleteProducers(Ider ider, int newProducerNumber) {
+        List<Producer> producers = producerDao.findLockByIdCodeOrderByIndexAsc(ider.getIdCode());
+        for (int i = newProducerNumber; i < ider.getProducerNumber(); i++) {
+            Producer deletingProducer = producers.get(i);
+            updateProducer(producers.get(i % ider.getProducerNumber()), deletingProducer);
+            producerDao.delete(deletingProducer);
+        }
+    }
+
+    // 更新id生产者
+    private void updateProducer(Producer producer, Producer deletingProducer) {
+        if (new ProducerUtils.ProducerComparator().compare(producer, deletingProducer) < 0) {
+            producer.setCurrentPeriod(deletingProducer.getCurrentPeriod());
+            producer.setCurrentId(deletingProducer.getCurrentId());
+            producerDao.save(producer);
+        }
     }
 }
---
+++ ids-biz/src/main/java/org/antframework/ids/biz/util/ProducerUtils.java
@@ -0,0 +1,87 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2017-11-27 00:34 创建
+ */
+package org.antframework.ids.biz.util;
+
+import org.antframework.ids.dal.entity.Ider;
+import org.antframework.ids.dal.entity.Producer;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Comparator;
+import java.util.Date;
+
+/**
+ * id生产者工具类
+ */
+public class ProducerUtils {
+
+    /**
+     * 生产id
+     *
+     * @param producer id生产者
+     * @param ider     id提供者
+     * @param amount   需生产的id个数
+     */
+    public static void produce(Producer producer, Ider ider, int amount) {
+        long newCurrentId = producer.getCurrentId() + amount * ider.getProducerNumber();
+        if (ider.getMaxId() != null && newCurrentId >= ider.getMaxId()) {
+            Date newCurrentPeriod = producer.getCurrentPeriod();
+            int carry = (int) (newCurrentId / ider.getMaxId());
+            switch (ider.getPeriodType()) {
+                case HOUR:
+                    newCurrentPeriod = DateUtils.addHours(newCurrentPeriod, carry);
+                    break;
+                case DAY:
+                    newCurrentPeriod = DateUtils.addDays(newCurrentPeriod, carry);
+                    break;
+                case MONTH:
+                    newCurrentPeriod = DateUtils.addMonths(newCurrentPeriod, carry);
+                    break;
+                case YEAR:
+                    newCurrentPeriod = DateUtils.addYears(newCurrentPeriod, carry);
+                    break;
+                default:
+                    throw new IllegalStateException(String.format("生产者[%s]剩余空间不足", producer.toString()));
+            }
+            producer.setCurrentPeriod(newCurrentPeriod);
+
+            newCurrentId %= ider.getMaxId();
+        }
+        producer.setCurrentId(newCurrentId);
+    }
+
+    /**
+     * id生产者比较器
+     */
+    public static class ProducerComparator implements Comparator<Producer> {
+
+        @Override
+        public int compare(Producer o1, Producer o2) {
+            if (!StringUtils.equals(o1.getIdCode(), o2.getIdCode())) {
+                throw new IllegalArgumentException(String.format("待比较的id生产者%s和%s不属于同一个id提供者，不能进行比较", o1.toString(), o2.toString()));
+            }
+            if (o1.getCurrentPeriod() != o2.getCurrentPeriod()) {
+                if (o1.getCurrentPeriod().getTime() < o2.getCurrentPeriod().getTime()) {
+                    return -1;
+                } else if (o1.getCurrentPeriod().getTime() == o2.getCurrentPeriod().getTime()) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+            if (o1.getCurrentId() < o2.getCurrentId()) {
+                return -1;
+            } else if (o1.getCurrentId() == o2.getCurrentId()) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
+}
