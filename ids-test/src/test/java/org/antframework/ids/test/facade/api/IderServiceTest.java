6cc756f9e9d402dda694effab1118150df6b3084
zhongxunking <zhongxunking@163.com>
2017-12-03 21:28:02 +0800

    获取批量id接口压测

--- ids-test/src/test/java/org/antframework/ids/test/facade/api/IderServiceTest.java
+++ ids-test/src/test/java/org/antframework/ids/test/facade/api/IderServiceTest.java
@@ -16,21 +16,59 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * id提供者服务单元测试
  */
 @Ignore
 public class IderServiceTest extends AbstractTest {
+    private ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
+            1000,
+            1000,
+            60,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue<Runnable>(10000));
     @Autowired
     private IderService iderService;
+    private int count = 10000;
 
     @Test
-    public void testAcquireIds() {
-        AcquireIdsOrder order = new AcquireIdsOrder();
-        order.setIdCode("oid");
-        order.setExpectAmount(100);
+    public void testAcquireIds() throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            threadPool.execute(new Task(startTime, i));
+        }
+        long endTime = System.currentTimeMillis();
 
-        AcquireIdsResult result = iderService.acquireIds(order);
-        assertSuccess(result);
+        System.out.println(String.format("添加任务耗时：%d毫秒", endTime - startTime));
+
+        Thread.sleep(100000);
+    }
+
+    public class Task implements Runnable {
+        private long startTime;
+        private int i;
+
+        public Task(long startTime, int i) {
+            this.startTime = startTime;
+            this.i = i;
+        }
+
+        @Override
+        public void run() {
+            AcquireIdsOrder order = new AcquireIdsOrder();
+            order.setIdCode("oid");
+            order.setExpectAmount(100);
+
+            AcquireIdsResult result = iderService.acquireIds(order);
+            assertSuccess(result);
+            if (i == count - 1) {
+                long endTime = System.currentTimeMillis();
+                System.out.println(String.format("耗时：%d毫秒", endTime - startTime));
+            }
+        }
     }
 }
