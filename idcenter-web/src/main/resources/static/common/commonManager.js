de1316ff0c91848bafc161f907685bcda9c80e9f
zhongxunking <zhongxunking@163.com>
2018-01-31 23:38:15 +0800

    管理页面

---
+++ idcenter-web/src/main/resources/static/common/commonManager.js
@@ -0,0 +1,28 @@
+var manager = null;
+$(document).ready(function () {
+    manager = getCurrentManager();
+    if (manager == null) {
+        alert("未登录，或登录超时，请重新登陆");
+        window.location.href = "login.html";
+    }
+});
+
+function getCurrentManager() {
+    var manager = null;
+    $.ajax({
+        url: "/manager/main/current",
+        data: {},
+        async: false,
+        success: function (ajaxResult) {
+            if (ajaxResult.success) {
+                manager = ajaxResult.manager;
+            } else {
+                alert("失败，原因：" + ajaxResult.message);
+            }
+        },
+        error: function () {
+            alert("请求服务失败");
+        }
+    });
+    return manager;
+}
