d2ed40448562e5fd04f9aca2746039825c9da138
zhongxunking <zhongxunking@163.com>
2021-05-09 19:42:22 +0800

    前端静态资源版本化管理

--- idcenter-web/src/main/resources/static/common/import.js
+++ idcenter-web/src/main/resources/static/common/import.js
@@ -1,3 +1,4 @@
-document.write('<script src="../manager/import.js"></script>');
-document.write('<script src="../common/common.js"></script>');
-document.write('<link rel="stylesheet" type="text/css" href="../lib/icon/iconfont.css">');
+IDCENTER_VERSION = '1.6.0-SNAPSHOT';
+document.write("<script src=\"../manager/import.js?version=" + IDCENTER_VERSION + "\"><\/script>");
+document.write("<script src=\"../common/common.js?version=" + IDCENTER_VERSION + "\"><\/script>");
+document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"../lib/icon/iconfont.css?version=" + IDCENTER_VERSION + "\">");
